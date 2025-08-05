c880381b319b7b5d16404163252c17148bed2d33
maxfromit <bigmaxcapacity@gmail.com>
2025-08-05 12:21:10 +0300

    fix: update ESLint configuration to warn on unused TypeScript variables

--- eslint.config.ts
+++ eslint.config.ts
@@ -19,7 +19,11 @@ export default defineConfigWithVueTs(
 
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
-  
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,11 @@
   <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
+    documentation 23123123
+  </p>
+  <p>
+    Visit also<a href="https://vuejs.org/also" target="_blank" rel="noopener">vuejs.org</a> to read
+    the documentation about also
   </p>
 </template>
 
