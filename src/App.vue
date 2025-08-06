fa7560b6482ef24e83a72363f51dc699a5e3a193
maxfromit <bigmaxcapacity@gmail.com>
2025-08-06 11:34:39 +0300

    feat(App): simplify App component by removing unnecessary documentation and integrating TreeGrid

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TreeGrid from './components/TreeGrid.vue'
+</script>
 
 <template>
   <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation 23123123
-  </p>
-  <p>
-    Visit also<a href="https://vuejs.org/also" target="_blank" rel="noopener">vuejs.org</a> to read
-    the documentation about also
-  </p>
+  <TreeGrid />
 </template>
 
 <style scoped></style>
