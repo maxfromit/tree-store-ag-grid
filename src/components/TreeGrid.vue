b4d529e7e90ea01b3a0a70326f6d2735a1e39423
maxfromit <bigmaxcapacity@gmail.com>
2025-08-06 17:46:12 +0300

    feat(TreeGrid): enhance TreeGrid component with improved column definitions and grid options and with styling. View mode is ready

--- src/components/TreeGrid.vue
+++ src/components/TreeGrid.vue
@@ -1,20 +1,122 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { AgGridVue } from 'ag-grid-vue3' // Vue Data Grid Component
-import { ModuleRegistry, AllCommunityModule } from 'ag-grid-community'
+import { ref, computed } from 'vue'
+import { AgGridVue } from 'ag-grid-vue3'
+import { ModuleRegistry, AllCommunityModule, type ColDef } from 'ag-grid-community'
+import { RowGroupingModule, TreeDataModule } from 'ag-grid-enterprise'
 import { TreeStore } from '@/store/TreeStore'
 import { items } from '@/constants/items'
+import 'ag-grid-enterprise'
+import { themeQuartz } from 'ag-grid-community'
+// import './style.css'
 
-ModuleRegistry.registerModules([AllCommunityModule])
+const myTheme = themeQuartz.withParams({
+  headerColumnBorder: true,
+  pinnedColumnBorder: false,
+})
+
+ModuleRegistry.registerModules([AllCommunityModule, RowGroupingModule, TreeDataModule])
 
 const treeStore = new TreeStore(items)
-console.log(treeStore.getAll())
-const rowData = ref(treeStore.getAll())
 
-// Column Definitions: Defines the columns to be displayed.
-const colDefs = ref([{ field: 'id' }, { field: 'parent' }, { field: 'label' }])
+const renderBoldIfParent = (
+  params: any,
+  customRegularText?: string,
+  customBoldText?: string,
+  allBold?: boolean,
+) => {
+  let makeBold = false
+  if (!allBold) {
+    const isChild = treeStore.getChildren(params.data.id).length === 0
+    makeBold = !isChild
+  }
+  if (allBold) {
+    makeBold = true
+  }
+  return makeBold
+    ? `<b>${customBoldText || `${params.value || ''}`}</b>`
+    : `${customRegularText || params.value || ''}`
+}
+
+const columnDefs = ref<ColDef[]>([
+  {
+    headerName: '№ п\\п',
+    valueGetter: 'node.sourceRowIndex + 1',
+    cellRenderer: (p) => renderBoldIfParent(p, undefined, undefined, true),
+    flex: 0,
+    width: 100,
+    pinned: 'left',
+  },
+  { field: 'parent', headerName: 'Категория', rowGroup: true, hide: true },
+  {
+    field: 'label',
+    headerName: 'Наименование',
+  },
+])
+
+const gridOptions = computed(() => ({
+  style: { headerRowBorder: true },
+
+  rowData: treeStore.getAll(),
+  theme: myTheme,
+  columnDefs: columnDefs.value,
+  defaultColDef: {
+    cellClass: 'logoCell',
+    cellRenderer: renderBoldIfParent,
+    resizable: false,
+    flex: 1,
+
+    // innerRenderer: (p) => {
+    //   console.log('pdata', p.data)
+    //   const isChild = treeStore.getChildren(p.data.id).length === 0
+    //   console.log('p', treeStore.getChildren(p.data.id))
+    //   return isChild ? 'Элемент' : '<b>Группа</b>'
+    // }, // Use the default group cell renderer
+  },
+  treeData: true,
+  treeDataParentIdField: 'parent',
+  // groupHideOpenParents: true,
+  // groupDisplayType: 'multipleColumn',
+  getRowId: (params: any) => params.data.id,
+
+  autoGroupColumnDef: {
+    headerName: 'Категория',
+    field: 'parent',
+    cellRendererParams: {
+      suppressCount: true,
+      innerRenderer: (p) => renderBoldIfParent(p, 'Элемент', 'Группа'),
+    },
+  },
+}))
 </script>
 
 <template>
-  <ag-grid-vue :rowData="rowData" :columnDefs="colDefs" style="height: 500px"> </ag-grid-vue>
+  <div class="tree-grid-wrapper">
+    <div class="tree-grid-header">Режим: просмотр</div>
+    <ag-grid-vue :gridOptions="gridOptions" class="ag-theme-quartz tree-grid-content" />
+  </div>
 </template>
+
+<style scoped lang="scss">
+.tree-grid-wrapper {
+  display: flex;
+  flex-direction: column;
+  height: 500px;
+  background-color: #ebebeb;
+  padding: 10px;
+  gap: 10px;
+}
+.tree-grid-header {
+  color: #4788e3;
+  padding: 10px;
+}
+
+.tree-grid-content {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  min-height: 0;
+  //  flex: 1;
+  // min-height: 0;
+  width: 100%;
+}
+</style>
