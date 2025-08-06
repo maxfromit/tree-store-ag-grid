1091047b241fb7afc32d1091e4d43fb80574a8cb
maxfromit <bigmaxcapacity@gmail.com>
2025-08-06 11:34:20 +0300

    feat(tests): refactor TreeStore tests and example to use external items constant

--- src/__tests__/TreeStore.spec.ts
+++ src/__tests__/TreeStore.spec.ts
@@ -1,5 +1,7 @@
 import { describe, it, expect, beforeEach } from 'vitest'
-import { TreeStore } from '../store/TreeStore'
+import { TreeStore } from '@/store/TreeStore'
+import { items } from '@/constants/items'
+
 
 interface Item {
   id: number | string
@@ -9,19 +11,9 @@ interface Item {
 
 describe('TreeStore', () => {
   let treeStore: TreeStore
-  let items: Item[]
 
   beforeEach(() => {
-    items = [
-      { id: 1, parent: null, label: 'Айтем 1' },
-      { id: '2', parent: 1, label: 'Айтем 2' },
-      { id: 3, parent: 1, label: 'Айтем 3' },
-      { id: 4, parent: '2', label: 'Айтем 4' },
-      { id: 5, parent: '2', label: 'Айтем 5' },
-      { id: 6, parent: '2', label: 'Айтем 6' },
-      { id: 7, parent: 4, label: 'Айтем 7' },
-      { id: 8, parent: 4, label: 'Айтем 8' },
-    ]
+
     treeStore = new TreeStore(items)
   })
 
--- src/example.ts
+++ src/example.ts
@@ -1,15 +1,5 @@
 import { TreeStore } from './store/TreeStore'
-
-const items = [
-  { id: 1, parent: null, label: 'Айтем 1' },
-  { id: '2', parent: 1, label: 'Айтем 2' },
-  { id: 3, parent: 1, label: 'Айтем 3' },
-  { id: 4, parent: '2', label: 'Айтем 4' },
-  { id: 5, parent: '2', label: 'Айтем 5' },
-  { id: 6, parent: '2', label: 'Айтем 6' },
-  { id: 7, parent: 4, label: 'Айтем 7' },
-  { id: 8, parent: 4, label: 'Айтем 8' },
-]
+import { items } from './constants/items'
 
 const treeStore = new TreeStore(items)
 
