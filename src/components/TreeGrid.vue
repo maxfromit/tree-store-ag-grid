1a70a5ab9fe11d9fafc015bdada917f43d43cab1
maxfromit <bigmaxcapacity@gmail.com>
2025-08-07 13:12:14 +0300

    feat(TreeGrid): enchanse further. Add tailwind classes,  vueuse to implement undo / redo on the whole treeStore, remove renderBold function in favor of using tailwind class depending on isGroup or not, add nextNumericId to get new id, fix classes for div in AgGrid to use h-full instead of h in pixels

--- src/components/TreeGrid.vue
+++ src/components/TreeGrid.vue
@@ -1,14 +1,25 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 import { AgGridVue } from 'ag-grid-vue3'
-import { ModuleRegistry, AllCommunityModule, type ColDef } from 'ag-grid-community'
+import { ModuleRegistry, AllCommunityModule } from 'ag-grid-community'
+import type {
+  ColDef,
+  ICellRendererParams,
+  CellClassParams,
+  NewValueParams,
+  GetRowIdParams,
+} from 'ag-grid-community'
 import { RowGroupingModule, TreeDataModule } from 'ag-grid-enterprise'
 import { TreeStore } from '@/store/TreeStore'
 import { items } from '@/constants/items'
 import 'ag-grid-enterprise'
 import { themeQuartz } from 'ag-grid-community'
 import { ArrowUturnRightIcon, ArrowUturnLeftIcon } from '@heroicons/vue/16/solid'
-// import './style.css'
+import TreeGroupCell from './components/TreeGroupCell.vue'
+import { isGroupByParams, getCategoryLabel } from './utils'
+import type { ItemId, Item as ItemBase, Item } from '@/store/TreeStore'
+import l from 'lodash'
+import { useRefHistory } from '@vueuse/core'
 
 const myTheme = themeQuartz.withParams({
   headerColumnBorder: true,
@@ -17,145 +28,216 @@ const myTheme = themeQuartz.withParams({
 
 ModuleRegistry.registerModules([AllCommunityModule, RowGroupingModule, TreeDataModule])
 
-const treeStore = new TreeStore(items)
+const treeStoreInitial = new TreeStore(items)
+const treeStore = ref(new TreeStore(items))
+
+const itemsRef = computed(() => treeStore.value.getAll())
+const { undo, redo } = useRefHistory(treeStore, { clone: l.cloneDeep, deep: true })
+
+const agGrid = ref(null)
+
 const mode = ref<'view' | 'edit'>('view')
+
 const toggleMode = () => {
   mode.value = mode.value === 'view' ? 'edit' : 'view'
 }
 
-const renderBoldIfParent = (
-  params: any,
-  customRegularText?: string,
-  customBoldText?: string,
-  allBold?: boolean,
-) => {
-  let makeBold = false
-  if (!allBold) {
-    const isChild = treeStore.getChildren(params.data.id).length === 0
-    makeBold = !isChild
-  }
-  if (allBold) {
-    makeBold = true
-  }
-  return makeBold
-    ? `<b>${customBoldText || `${params.value || ''}`}</b>`
-    : `${customRegularText || params.value || ''}`
+const applyBoldForGroup = (params: CellClassParams) => {
+  console.log('applyBoldForGroup called with params:', params?.node?.group)
+  // const isGroup = isGroupByParams(params)
+  return isGroupByParams(params) ? 'font-bold' : ''
 }
 
 const columnDefs = ref<ColDef[]>([
   {
     headerName: '№ п\\п',
     valueGetter: 'node.sourceRowIndex + 1',
-    cellRenderer: (p) => renderBoldIfParent(p, undefined, undefined, true),
     flex: 0,
     width: 100,
     pinned: 'left',
+    cellClass: 'font-bold',
   },
-  { field: 'parent', headerName: 'Категория', rowGroup: true, hide: true },
+  {
+    field: 'parent',
+    headerName: 'Категория',
+    rowGroup: true,
+    hide: true,
+  },
   {
     field: 'label',
     headerName: 'Наименование',
+    editable: true,
+
+    cellClass: (params: CellClassParams) => {
+      return applyBoldForGroup(params) + ' cursor-text hover:text-blue-500 focus:text-blue-600'
+    },
+    onCellValueChanged: handleCellValueChanged,
   },
 ])
 
-const gridOptions = computed(() => ({
-  style: { headerRowBorder: true },
+// function refreshGrid() {
+//   if (agGrid.value && agGrid.value.api) {
+//     agGrid.value.api.refreshClientSideRowModel('group')
+//     // or, for a full refresh:
+//     agGrid.value.api.setRowData(history.value)
+//   }
+// }
 
-  rowData: treeStore.getAll(),
+const handleAdd = (item: Omit<ItemBase, 'id'>) => {
+  console.log('handleAdd called with item:', item)
+  treeStore.value.addItem({
+    id: nextNumericId.value,
+    ...item,
+  })
+  // refreshGrid()
+}
+
+export type HandleAdd = typeof handleAdd
+export type HandleDelete = typeof handleDelete
+
+const getAutoGroupColumnDef = () => {
+  return {
+    cellClass: applyBoldForGroup,
+    cellRendererParams: {
+      suppressCount: true,
+
+      ...(mode.value === 'edit'
+        ? { innerRenderer: TreeGroupCell }
+        : {
+            innerRenderer: getCategoryLabel,
+          }),
+
+      // ...(mode.value === 'edit'
+      //   ? { innerRenderer: TreeGroupCell }
+      //   : {
+      //       innerRenderer: (p) =>
+      //         renderBoldIfParent({
+      //           params: p,
+      //           customRegular: 'Элемент',
+      //           customBold: 'Группа',
+      //         }),
+      //     }),
+
+      innerRendererParams: {
+        action: {
+          add: handleAdd,
+          delete: handleDelete,
+        },
+      },
+    },
+  }
+}
+
+const handleDelete = (id: ItemId) => {
+  console.log('handleDelete called with id:', id)
+  treeStore.value.removeItem(id)
+  // refreshItems()
+}
+const getRowId = (params: GetRowIdParams<Item, any>) => {
+  return l.toString(params.data.id)
+}
+
+function handleCellValueChanged(event: NewValueParams<Item, string, any>) {
+  console.log('handleCellValueChanged called with event:', event)
+  if (event.colDef.field === 'label' && l.isString(event.newValue) && !l.isEmpty(event.newValue)) {
+    console.log('Updating item with new label:', event.newValue)
+    treeStore.value.updateItem({
+      ...event.data,
+      label: event.newValue,
+    })
+  }
+}
+
+const gridOptions = computed(() => ({
+  // rowData: itemsClone.value,
   theme: myTheme,
   columnDefs: columnDefs.value,
   defaultColDef: {
-    cellClass: 'logoCell',
-    cellRenderer: renderBoldIfParent,
+    // cellRenderer: renderBoldIfParent,
     resizable: false,
     flex: 1,
-
-    // innerRenderer: (p) => {
-    //   console.log('pdata', p.data)
-    //   const isChild = treeStore.getChildren(p.data.id).length === 0
-    //   console.log('p', treeStore.getChildren(p.data.id))
-    //   return isChild ? 'Элемент' : '<b>Группа</b>'
-    // }, // Use the default group cell renderer
+    sortable: false,
   },
   treeData: true,
   treeDataParentIdField: 'parent',
-  // groupHideOpenParents: true,
-  // groupDisplayType: 'multipleColumn',
-  getRowId: (params: any) => params.data.id,
+  getRowId: (params: GetRowIdParams<Item, any>) => l.toString(params.data.id),
+  autoGroupColumnDef: getAutoGroupColumnDef(),
+  // autoGroupColumnDef: {
+  //   // headerName: 'Категория',
+  //   // field: 'parent',
+  //   // cellRenderer: TreeGroupCell,
+  //   cellRendererParams: {
+  //     suppressCount: true,
+  //     // innerRendererFramework: TreeGroupCell,
 
-  autoGroupColumnDef: {
-    headerName: 'Категория',
-    field: 'parent',
-    cellRendererParams: {
-      suppressCount: true,
-      innerRenderer: (p) => renderBoldIfParent(p, 'Элемент', 'Группа'),
-    },
-  },
+  //     ...(mode.value === 'edit'
+  //       ? { innerRenderer: TreeGroupCell }
+  //       : {
+  //           innerRenderer: (p) =>
+  //             renderBoldIfParent({
+  //               params: p,
+  //               customRegular: 'Элемент',
+  //               customBold: 'Группа',
+  //             }),
+  //         }),
+
+  //     // innerRenderer: (p) => renderBoldIfParent(p, 'Элемент', 'Группа'),
+  //     // innerRenderer: mode.value === 'edit' ? TreeGroupCell : undefined,
+
+  //     innerRendererParams: {
+  //       action: {
+  //         add: handleAdd,
+  //         delete: handleDelete,
+  //       },
+  //     },
+  //   },
+  // },
 }))
+
+const nextNumericId = computed(() => {
+  const all = treeStore.value.getAll()
+  const numericIds = l.filter(
+    l.map(all, (item: Item) => (l.isNumber(item.id) ? item.id : null)),
+    l.isNumber,
+  )
+  return !l.isEmpty(numericIds) ? (l.max(numericIds) ?? 0) + 1 : 1
+})
 </script>
 
 <template>
-  <div class="tree-grid-wrapper">
-    <div class="tree-grid-header flex-row-centered">
+  <div class="flex flex-col h-screen bg-gray-200 p-2 gap-2">
+    itemsRef {{ itemsRef }}
+    <div class="flex flex-row items-center gap-2 text-blue-500 p-2">
       <div>
         Режим:
-        <span @click="toggleMode" class="cursor-pointer">
+        <span
+          @click="toggleMode"
+          class="cursor-pointer hover:text-blue-600 focus:text-blue-700 transition-colors"
+        >
           {{ mode === 'view' ? 'просмотр' : 'редактирование' }}
         </span>
       </div>
-      <div v-if="mode === 'edit'" class="flex-row-centered gap-5">
-        <ArrowUturnLeftIcon class="icon" />
-        <ArrowUturnRightIcon class="icon" />
+      <div v-if="mode === 'edit'" class="flex flex-row items-center gap-1">
+        <button type="button" @click="undo">
+          <ArrowUturnLeftIcon class="w-4 h-4 hover:text-blue-600 focus:text-blue-700" />
+        </button>
+        <button type="button" @click="redo">
+          <ArrowUturnRightIcon class="w-4 h-4 hover:text-blue-600 focus:text-blue-700" />
+        </button>
       </div>
     </div>
-    <ag-grid-vue :gridOptions="gridOptions" class="ag-theme-quartz tree-grid-content" />
+
+    <ag-grid-vue
+      ref="agGrid"
+      :rowData="itemsRef"
+      :theme="gridOptions.theme"
+      :columnDefs="gridOptions.columnDefs"
+      :defaultColDef="gridOptions.defaultColDef"
+      :treeData="gridOptions.treeData"
+      :treeDataParentIdField="gridOptions.treeDataParentIdField"
+      :getRowId="getRowId"
+      :autoGroupColumnDef="getAutoGroupColumnDef()"
+      class="ag-theme-quartz flex-1 min-h-0 flex flex-col"
+    />
   </div>
 </template>
-
-<style scoped lang="scss">
-.icon {
-  width: 18px;
-  height: 18px;
-  cursor: pointer;
-  // color: #222;
-  // margin-left: 12px;
-}
-
-.gap-5 {
-  gap: 5px;
-}
-.tree-grid-wrapper {
-  display: flex;
-  flex-direction: column;
-  height: 500px;
-  background-color: #ebebeb;
-  padding: 10px;
-  gap: 10px;
-}
-
-.flex-row-centered {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-}
-
-.tree-grid-header {
-  gap: 10px;
-  color: #4788e3;
-  padding: 10px;
-}
-
-.cursor-pointer {
-  cursor: pointer;
-}
-
-.tree-grid-content {
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  min-height: 0;
-  //  flex: 1;
-  // min-height: 0;
-  width: 100%;
-}
-</style>
