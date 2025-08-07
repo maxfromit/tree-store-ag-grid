687cea96a882fbbd6a35cf11d58795055712c6e0
maxfromit <bigmaxcapacity@gmail.com>
2025-08-07 11:23:52 +0300

    refactor(utils): improve type safety for isGroupByParams and add getCategoryLabel function

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,1 +1,11 @@
-export const isGroupByParams = (params: any) => params.node && params.node.group
+import type { ICellRendererParams, CellClassParams } from 'ag-grid-community'
+
+export const isGroupByParams = (params: ICellRendererParams | CellClassParams) =>
+  params.node && params.node.group
+
+export const getCategoryLabel = (params: ICellRendererParams) => {
+  if (isGroupByParams(params)) {
+    return 'Группа'
+  }
+  return 'Элемент'
+}
