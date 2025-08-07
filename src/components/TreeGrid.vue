978a6f17a269a0f49945d273c26c5b6ce2b22ba1
maxfromit <bigmaxcapacity@gmail.com>
2025-08-07 17:44:31 +0300

    refactor(TreeGrid): streamline code by grouping all gridOptions in gridOptions and add composable for dialog and adding

--- src/components/TreeGrid.vue
+++ src/components/TreeGrid.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
-import { ref, computed, shallowRef } from 'vue'
+import { ref, computed, shallowRef, type Ref } from 'vue'
 import l from 'lodash'
 import { AgGridVue } from 'ag-grid-vue3'
 import { ModuleRegistry, AllCommunityModule } from 'ag-grid-community'
 import type {
-  ColDef,
   ICellRendererParams,
   CellClassParams,
   NewValueParams,
   GetRowIdParams,
   GridReadyEvent,
   GridApi,
-  ValueFormatterParams,
 } from 'ag-grid-community'
 import { RowGroupingModule, TreeDataModule, RowNumbersModule } from 'ag-grid-enterprise'
 import { TreeStore } from '@/store/TreeStore'
@@ -50,77 +48,59 @@ const toggleMode = () => {
   mode.value = mode.value === 'view' ? 'edit' : 'view'
 }
 
-const applyBoldForGroup = (params: CellClassParams) => {
-  return isParent(params.data.id) ? 'font-bold' : ''
-}
+const applyBoldForGroup = (params: CellClassParams) => (isParent(params.data.id) ? 'font-bold' : '')
 
-const valueFormatter = (params: ValueFormatterParams) => {
-  return params.value
-}
+const { isDialogShown, getParentAndShowNewItemLabelDialog, dismissDialog, addItem } =
+  useAddWithDialog()
 
-const columnDefs = ref<ColDef[]>([
-  {
-    field: 'parent',
-    headerName: 'Категория',
-    rowGroup: true,
-    hide: true,
-  },
-  {
-    field: 'label',
-    headerName: 'Наименование',
-    editable: mode.value === 'edit' ? true : false,
+const deleteItem = (id: ItemId) => treeStore.value.removeItem(id)
 
-    cellClass: (params: CellClassParams) => {
-      return (
-        applyBoldForGroup(params) +
-        (mode.value === 'edit' ? ' cursor-text hover:text-blue-500 focus:text-blue-600' : '')
-      )
+const gridOptions = computed(() => ({
+  theme: myTheme,
+  columnDefs: [
+    {
+      field: 'parent',
+      headerName: 'Категория',
+      rowGroup: true,
+      hide: true,
     },
-    onCellValueChanged: handleCellValueChanged,
+    {
+      field: 'label',
+      headerName: 'Наименование',
+      editable: mode.value === 'edit' ? true : false,
+
+      cellClass: (params: CellClassParams) => {
+        return (
+          applyBoldForGroup(params) +
+          (mode.value === 'edit' ? ' cursor-text hover:text-blue-500 focus:text-blue-600' : '')
+        )
+      },
+      onCellValueChanged: (event: NewValueParams<Item, string, any>) => {
+        if (l.isString(event.newValue)) {
+          treeStore.value.updateItem({
+            ...event.data,
+            label: event.newValue,
+          })
+        }
+      },
+    },
+  ],
+
+  defaultColDef: {
+    // valueFormatter,
+    // cellRenderer: renderBoldIfParent,
+    resizable: false,
+    flex: 1,
+    sortable: false,
   },
-])
 
-const rowNumbersFormatter = (params: ValueFormatterParams) => {
-  return params?.value
-}
+  treeData: true,
+  treeDataParentIdField: 'parent',
+  getRowId: (params: GetRowIdParams<Item, any>) => {
+    return l.toString(params.data.id)
+  },
 
-const isDialogShown = ref(false)
-
-const addItem = async (label: string) => {
-  const newItem = {
-    id: nextNumericId.value,
-    label,
-    parent: newItemParent.value,
-  }
-  treeStore.value.addItem(newItem)
-  const node = gridApi?.value?.getRowNode(l.toString(newItem.parent))
-  dismissDialog()
-
-  await new Promise((resolve) => setTimeout(resolve, 300))
-  if (node) {
-    gridApi?.value?.setRowNodeExpanded(node, true, true)
-  }
-}
-
-const newItemParent = ref<ItemId | null>(null)
-const showNewItemLabelDialog = (parent: ItemId) => {
-  isDialogShown.value = true
-  newItemParent.value = parent
-  // emit('show-dialog', parent)
-
-  // refreshGrid()
-}
-
-const dismissDialog = () => {
-  isDialogShown.value = false
-}
-
-export type ShowNewItemLabelDialog = typeof showNewItemLabelDialog
-export type DeleteItem = typeof deleteItem
-export type IsParent = typeof isParent
-
-const getAutoGroupColumnDef = () => {
-  return {
+  autoGroupColumnDef: {
     cellClass: applyBoldForGroup,
     cellRendererParams: {
       suppressCount: true,
@@ -135,59 +115,65 @@ const getAutoGroupColumnDef = () => {
       innerRendererParams: {
         isParent: isParent,
         action: {
-          showDialog: showNewItemLabelDialog,
+          showDialog: getParentAndShowNewItemLabelDialog,
           delete: deleteItem,
         },
       },
     },
-  }
-}
-
-const deleteItem = (id: ItemId) => {
-  console.log('handleDelete called with id:', id)
-  treeStore.value.removeItem(id)
-  // refreshItems()
-}
-const getRowId = (params: GetRowIdParams<Item, any>) => {
-  return l.toString(params.data.id)
-}
-
-function handleCellValueChanged(event: NewValueParams<Item, string, any>) {
-  console.log('handleCellValueChanged called with event:', event)
-  if (event.colDef.field === 'label' && l.isString(event.newValue) && !l.isEmpty(event.newValue)) {
-    console.log('Updating item with new label:', event.newValue)
-    treeStore.value.updateItem({
-      ...event.data,
-      label: event.newValue,
-    })
-  }
-}
-
-const gridOptions = computed(() => ({
-  // rowData: itemsClone.value,
-  theme: myTheme,
-  columnDefs: columnDefs.value,
-  defaultColDef: {
-    valueFormatter,
-    // cellRenderer: renderBoldIfParent,
-    resizable: false,
-    flex: 1,
-    sortable: false,
   },
-  treeData: true,
-  treeDataParentIdField: 'parent',
-  getRowId: (params: GetRowIdParams<Item, any>) => l.toString(params.data.id),
-  autoGroupColumnDef: getAutoGroupColumnDef(),
+  rowNumbers: true,
 }))
 
-const nextNumericId = computed(() => {
-  const all = treeStore.value.getAll()
-  const numericIds = l.filter(
-    l.map(all, (item: Item) => (l.isNumber(item.id) ? item.id : null)),
-    l.isNumber,
-  )
-  return !l.isEmpty(numericIds) ? (l.max(numericIds) ?? 0) + 1 : 1
-})
+function useAddWithDialog() {
+  const isDialogShown = ref(false)
+
+  const dismissDialog = () => {
+    isDialogShown.value = false
+  }
+
+  const newItemParent = ref<ItemId | null>(null)
+
+  const getParentAndShowNewItemLabelDialog = (parent: ItemId) => {
+    newItemParent.value = parent
+    isDialogShown.value = true
+  }
+
+  const addItem = async (label: string) => {
+    const newItem = {
+      id: nextNumericId.value,
+      label,
+      parent: newItemParent.value,
+    }
+    treeStore.value.addItem(newItem)
+    const node = gridApi?.value?.getRowNode(l.toString(newItem.parent))
+    dismissDialog()
+
+    await new Promise((resolve) => setTimeout(resolve, 300))
+    if (node) {
+      gridApi?.value?.setRowNodeExpanded(node, true, true)
+    }
+  }
+
+  const nextNumericId = computed(() => {
+    const all = treeStore.value.getAll()
+    const numericIds = l.filter(
+      l.map(all, (item: Item) => (l.isNumber(item.id) ? item.id : null)),
+      l.isNumber,
+    )
+    return !l.isEmpty(numericIds) ? (l.max(numericIds) ?? 0) + 1 : 1
+  })
+
+  return {
+    isDialogShown,
+    dismissDialog,
+    getParentAndShowNewItemLabelDialog,
+    addItem,
+  }
+}
+
+export type ShowNewItemLabelDialog = typeof getParentAndShowNewItemLabelDialog
+export type DeleteItem = typeof deleteItem
+export type IsParent = typeof isParent
 </script>
 
 <template>
@@ -220,9 +206,9 @@ const nextNumericId = computed(() => {
       :defaultColDef="gridOptions.defaultColDef"
       :treeData="gridOptions.treeData"
       :treeDataParentIdField="gridOptions.treeDataParentIdField"
-      :getRowId="getRowId"
-      :autoGroupColumnDef="getAutoGroupColumnDef()"
-      :rowNumbers="{ headerName: '№ п\\п', valueFormatter: rowNumbersFormatter }"
+      :getRowId="gridOptions.getRowId"
+      :autoGroupColumnDef="gridOptions.autoGroupColumnDef"
+      :rowNumbers="gridOptions.rowNumbers"
       class="ag-theme-quartz flex-1 min-h-0 flex flex-col"
     />
   </div>
